b333afc056eff2e674bff90f23ceeb9562c5647c
warnotte <renaud@warnotte.be>
2016-01-07 11:24:29 +0100

    first commit

---
+++ src/main/java/com/example/user/WaxPhoneMartyrisator/MainActivity.java
@@ -0,0 +1,313 @@
+package com.example.user.WaxPhoneMartyrisator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.user.phonematique.R;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Thread thread_work;
+    ProgressBar progressBar;
+    TextView textArea;
+    Button button_send;
+    Button button_cancel;
+
+    String number = "0479708049";
+    String message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus";
+    boolean dummyMessageOrUserMessage = true;
+    int amount = 5;
+    boolean makeBeep = false; // Fait beep a chaque envois.
+    boolean testMode = true; // Si oui on envois rien du tout.
+    int delayBefore = 2000;
+    int delayBetween = 2000;
+
+    private EditText edit_text_numero;
+    private EditText edit_text_message;
+    private CheckBox chkBox_randomOrUserMsg;
+    private EditText edit_text_amount;
+    private CheckBox chkBox_testMode;
+    private CheckBox chkBox_makeBeep;
+    private EditText edit_text_delayBefore;
+    private EditText edit_text_delayBetween;
+
+    View parameterPanel;
+    ScrollView scroll_view_log;
+    LinearLayout paneau_progression;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        button_send = (Button) findViewById(R.id.buttonsend);
+        //button_quit.setText("Quit");
+        button_send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                parameterPanel.setVisibility(View.GONE);
+                doAction();
+            }
+        });
+
+        button_cancel = (Button) findViewById(R.id.buttonCancel);
+        //button_quit.setText("Quit");
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (thread_work!=null) {
+                    thread_work.interrupt();
+                    parameterPanel.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        textArea = (TextView) findViewById(R.id.editText);
+        textArea.setMovementMethod(new ScrollingMovementMethod());
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        scroll_view_log = (ScrollView)findViewById(R.id.scrollView2);
+
+        paneau_progression = (LinearLayout)findViewById(R.id.linearLayout2);
+        paneau_progression.setVisibility(View.GONE);
+        
+
+        initDateAndUI();
+
+        button_cancel.requestFocus();
+
+
+        parameterPanel = findViewById(R.id.parameterpanel);
+
+
+    }
+
+    private void initDateAndUI() {
+        edit_text_numero = (EditText) findViewById(R.id.editText_Numero);
+        edit_text_numero.setText(number);
+        edit_text_numero.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String txt = edit_text_numero.getText().toString();
+                Log.d("Change number", txt);
+                number = txt;
+            }
+        });
+
+        edit_text_message = (EditText) findViewById(R.id.editText_Message);
+        edit_text_message.setText(message);
+        edit_text_message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String txt = edit_text_message.getText().toString();
+                Log.d("Change message", txt);
+                message = txt;
+            }
+        });
+
+
+        chkBox_randomOrUserMsg = (CheckBox) findViewById(R.id.checkBox_RandomMsgOrUserMsg);
+        chkBox_randomOrUserMsg.setChecked(dummyMessageOrUserMessage);
+        chkBox_randomOrUserMsg.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean txt = chkBox_randomOrUserMsg.isChecked();
+                Log.d("Change message type", "" + txt);
+                dummyMessageOrUserMessage = txt;
+            }
+        });
+
+
+        edit_text_amount = (EditText) findViewById(R.id.editText_Amount);
+        edit_text_amount.setText("" + amount);
+        edit_text_amount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String txt = edit_text_amount.getText().toString();
+                Log.d("Change message", txt);
+                try {
+                    amount = Integer.parseInt(txt);
+                }
+                catch(NumberFormatException e)
+                {
+
+                }
+            }
+        });
+
+
+        chkBox_makeBeep = (CheckBox) findViewById(R.id.checkBox_MakeBeep);
+        chkBox_makeBeep.setChecked(makeBeep);
+        chkBox_makeBeep.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean txt = chkBox_makeBeep.isChecked();
+                Log.d("Change makeBeep type", "" + txt);
+                makeBeep = txt;
+            }
+        });
+
+        chkBox_testMode = (CheckBox) findViewById(R.id.checkBox_TestMode);
+        chkBox_testMode.setChecked(testMode);
+        chkBox_testMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean txt = chkBox_testMode.isChecked();
+                Log.d("Change testMode type", "" + txt);
+                testMode = txt;
+            }
+        });
+
+
+
+        edit_text_delayBefore = (EditText) findViewById(R.id.editText_DelayBefroe);
+        edit_text_delayBefore.setText("" + delayBefore);
+        edit_text_delayBefore.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String txt = edit_text_delayBefore.getText().toString();
+                Log.d("Change delayBefore", txt);
+                try {
+                    delayBefore = Integer.parseInt(txt);
+                }
+                catch(NumberFormatException e)
+                {
+
+                }
+            }
+        });
+
+        edit_text_delayBetween = (EditText) findViewById(R.id.editText_DelayBeetween);
+        edit_text_delayBetween.setText("" + delayBetween);
+        edit_text_delayBetween.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String txt = edit_text_delayBetween.getText().toString();
+                Log.d("Change message", txt);
+                try {
+                    delayBetween = Integer.parseInt(txt);
+                }
+                catch(NumberFormatException e)
+                {
+
+                }
+            }
+        });
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doAction() {
+
+        if (thread_work!=null)
+            thread_work.interrupt();
+
+        thread_work = new ThreadSend(this);
+        paneau_progression.setVisibility(View.VISIBLE);
+        button_send.setEnabled(false);
+        thread_work.start();
+
+    }
+
+
+}
---
+++ src/main/java/com/example/user/WaxPhoneMartyrisator/ThreadSend.java
@@ -0,0 +1,157 @@
+package com.example.user.WaxPhoneMartyrisator;
+
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Random;
+
+/**
+ * Created by user on 6/01/2016.
+ */
+public class ThreadSend extends Thread {
+
+    MainActivity ui;
+
+    volatile boolean running = true;
+
+    public ThreadSend(MainActivity ui) {
+        this.ui = ui;
+    }
+
+    public void run() {
+        Log.d("doAction", "start");
+
+        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+        //toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+
+        /* CALL A NUMERO
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:043669621"));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);*/
+
+
+        SmsManager smsManager = SmsManager.getDefault();
+
+        ui.progressBar.setMax(ui.amount);
+
+        setText("Waiting " + ui.delayBefore + " milliseconds...\r\n");
+        try {
+            Thread.sleep(ui.delayBefore);
+        } catch (InterruptedException e) {
+            appendText("Canceled\r\n");
+            enableSendButton();
+            return;
+        }
+
+        appendText("Starting...\r\n");
+
+
+        for (int i = 0; i < ui.amount; i++) {
+            if (running == false) break;
+            final int j = i;
+
+            if (ui.makeBeep==true)
+                toneG.startTone(ToneGenerator.TONE_CDMA_NETWORK_USA_RINGBACK, 25);
+            if (ui.testMode==false) {
+                if (ui.dummyMessageOrUserMessage==true) {
+                    String msg = generate();
+                    appendText("Message is "+msg+"...\r\n");
+                    smsManager.sendTextMessage(ui.number, null, msg, null, null);
+                }
+                else {
+                    String msg = ui.message;
+                    appendText("Message is "+msg+"...\r\n");
+                    smsManager.sendTextMessage(ui.number, null, msg, null, null);
+                }
+            }
+
+            appendText("Send message " + (j + 1) + "/" + ui.amount + " to " + ui.number + "\r\n");
+            setProgress(j + 1);
+
+            try {
+                Thread.sleep(ui.delayBetween);
+            } catch (InterruptedException e) {
+
+                appendText("Canceled\r\n");
+                enableSendButton();
+
+                return;
+            }
+            //SystemClock.sleep(1000);
+        }
+
+        appendText("Finished\r\n");
+        enableSendButton();
+        Log.d("doAction", "stop");
+    }
+
+    private void enableSendButton() {
+        ui.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ui.parameterPanel.setVisibility(View.VISIBLE);
+                ui.button_send.setEnabled(true);
+                ui.progressBar.setProgress(0);
+                ui.paneau_progression.setVisibility(View.GONE);
+                ui.scroll_view_log.fullScroll(View.FOCUS_UP);
+            }
+        });
+    }
+
+    public void setProgress(final int progress) {
+        ui.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ui.progressBar.setProgress(progress);
+            }
+        });
+    }
+
+    public void appendText(final String text) {
+        ui.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ui.textArea.append(text);
+                ui.scroll_view_log.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    public void setText(final String text) {
+        ui.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ui.textArea.setText(text);
+                ui.scroll_view_log.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+
+    public String generate()
+    {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 64; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        String output = sb.toString();
+        return output;
+    }
+}
